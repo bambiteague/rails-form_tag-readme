5f1fafdb5d7a75c4d4a949d4b42d77b933277b45
bambi teague <teeegsa@gmail.com>
2021-06-16 17:34:43 -0500

    commit tree fa386d3a27bd4ea1d40027d0e38ca4fb9f891dcf

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts, only: [:index,:new, :create] 
+  resources :posts, only: [:index, :create] 
+  get "/posts/new", to: "posts#new", as: "new_post_path"
 
 end
